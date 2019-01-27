c3ec0b193bd8d6367af97d89a8b8b97d57da810c
lucedeswede <nathanielcarolina@gmail.com>
2019-01-27 13:39:41 +0800

    Generate scaffold_controller for Activity

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'site/home'
   root 'site#home'
+  resources :activities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
