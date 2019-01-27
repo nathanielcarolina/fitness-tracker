7dc1fd44f36b94f047f49e78fb7a42259c737d0d
lucedeswede <nathanielcarolina@gmail.com>
2019-01-27 16:05:16 +0800

    Create initial views

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -69,6 +69,6 @@ class ActivitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def activity_params
-      params.fetch(:activity, {})
+      params.fetch(:activity, {}).permit(:name, :date, :duration)
     end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,5 @@
 class Activity < ApplicationRecord
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :duration, presence: true
 end
